6c04827c2cff77a60c7573c0a9fe81b0323e7880
bmiguel- <bmiguel-@student.42lisboa.com>
2021-11-29 22:35:17 +0000

    submodules

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: bmiguel- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 22:24:46 by bmiguel-          #+#    #+#             */
-/*   Updated: 2021/11/29 22:28:04 by bmiguel-         ###   ########.fr       */
+/*   Updated: 2021/11/29 22:34:43 by bmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ft_putnbr_base(long long int nbr, char *base)
 {
-	if (n < 0)
+	if (nbr < 0)
 	{
-		n *= -1;
+		nbr *= -1;
 		write(1, "-", 1);
 	}
-	if (nbr >= ft_strlen(base))
+	if (nbr >= (long long int)ft_strlen(base))
 		ft_putnbr_base(nbr / ft_strlen(base), base);
-	write(1, &base[nbr % / ft_strlen(base)], 1)
+	write(1, &base[nbr % ft_strlen(base)], 1);
 }
